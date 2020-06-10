dc09811698237ff9fe1dc17715f72ab3c6f0abb5
yangsh <yangsh@senthink.com>
2020-06-10 17:25:43 +0800

    :sparkles: 本地绝对订阅

--- src/mqtt/sub/hash.go
+++ src/mqtt/sub/hash.go
@@ -8,34 +8,23 @@ import (
 topic的绝对订阅哈希表
 */
 
-var clusterSub *clusterHashSub
+var localHashSub *hashSub
 
 func init() {
-	clusterSub = new(clusterHashSub)
-	clusterSub.nodesHashSub = make(map[string]*nodeHashSub)
+	localHashSub = newHashSub()
 }
 
 /**
 一个节点下的订阅关系
 */
-type nodeHashSub struct {
-	nodeName string
-	sub      map[string]map[string]bool
-	mutex    sync.RWMutex
+type hashSub struct {
+	sub   map[string]map[string]bool
+	mutex sync.RWMutex
 }
 
-/**
-集群下所有节点的订阅关系
-*/
-type clusterHashSub struct {
-	nodesHashSub map[string]*nodeHashSub
-	mutex        sync.Mutex
-}
+func newHashSub() *hashSub {
 
-func newNodeHashSub(nodeName string) *nodeHashSub {
-
-	n := new(nodeHashSub)
-	n.nodeName = nodeName
+	n := new(hashSub)
 	n.sub = make(map[string]map[string]bool)
 	return n
 }
@@ -43,79 +32,42 @@ func newNodeHashSub(nodeName string) *nodeHashSub {
 /**
 一个节点下的客户端订阅一个主题
 */
-func AddHashSub(topic, nodeName, clientIdentifier string) {
+func AddHashSub(topic, clientIdentifier string) {
 
-	var nodeHashSub *nodeHashSub
-	var ok bool
-	if nodeHashSub, ok = clusterSub.nodesHashSub[nodeName]; ok == false {
-		clusterSub.mutex.Lock()
-		if nodeHashSub, ok = clusterSub.nodesHashSub[nodeName]; ok == false {
-			clusterSub.nodesHashSub[nodeName] = newNodeHashSub(nodeName)
-			nodeHashSub = clusterSub.nodesHashSub[nodeName]
-		}
-		clusterSub.mutex.Unlock()
+	localHashSub.mutex.Lock()
+
+	if _, ok := localHashSub.sub[topic]; ok == false {
+		localHashSub.sub[topic] = make(map[string]bool)
 	}
-
-	nodeHashSub.mutex.Lock()
-	nodeHashSub.sub[topic][clientIdentifier] = true
-	nodeHashSub.mutex.Unlock()
-
+	localHashSub.sub[topic][clientIdentifier] = true
+	localHashSub.mutex.Unlock()
 }
 
 /**
 删除某一个节点的客户端订阅
 */
-func DelHash(topic, nodeName , clientIdentifier string) {
-	var nodeHashSub *nodeHashSub
-	var ok bool
-	if nodeHashSub, ok = clusterSub.nodesHashSub[nodeName]; ok == false {
-		clusterSub.mutex.Lock()
-		if nodeHashSub, ok = clusterSub.nodesHashSub[nodeName]; ok == true {
-			clusterSub.mutex.Unlock()
-			nodeHashSub.mutex.Lock()
-			delete(nodeHashSub.sub[topic], clientIdentifier)
-			nodeHashSub.mutex.Unlock()
-		}
-	} else {
-		nodeHashSub.mutex.Lock()
-		delete(nodeHashSub.sub[topic], clientIdentifier)
-		nodeHashSub.mutex.Unlock()
-	}
+func DeleteHashSub(topic, clientIdentifier string) {
 
+	localHashSub.mutex.Lock()
+	if _, ok := localHashSub.sub[topic]; ok {
+		delete(localHashSub.sub[topic], clientIdentifier)
+	}
+	localHashSub.mutex.Unlock()
 }
 
 /**
 获取不同节点的订阅客户端uuid
 */
-func GetHashSub(topic string) map[string][]string {
+func GetHashSub(topic string) []string {
 
-	nodeClients := make(map[string][]string)
-
-	for nodeName, hash := range clusterSub.nodesHashSub {
-		nodeClients[nodeName] = make([]string, 0, 20)
-		hash.mutex.RLock()
-		for id, _ := range hash.sub[topic] {
-			nodeClients[nodeName] = append(nodeClients[nodeName], id)
-		}
-		hash.mutex.RUnlock()
-
-	}
-
-	return nodeClients
-}
-
-/**
-获取一个节点下的topic订阅客户端标识符
-*/
-func GetANodeHashSub(topic, nodeName string) []string {
 	nodeClients := make([]string, 0)
-	if nodeSub, ok := clusterSub.nodesHashSub[nodeName]; ok {
-		nodeSub.mutex.RLock()
-		for clientIdentifier, _ := range nodeSub.sub[topic] {
-			nodeClients = append(nodeClients, clientIdentifier)
-		}
-		nodeSub.mutex.RUnlock()
-	}
 
+	localHashSub.mutex.RLock()
+
+	if v, ok := localHashSub.sub[topic]; ok {
+		for c, _ := range v {
+			nodeClients = append(nodeClients, c)
+		}
+	}
 	return nodeClients
 }
