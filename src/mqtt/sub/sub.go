e2b5edc53f38500d88c5e7a69fe1f6d4b3960635
yangsh <yangsh@senthink.com>
2020-06-12 16:59:10 +0800

    :art: 订阅与获取订阅客户端统一入口

---
+++ src/mqtt/sub/sub.go
@@ -0,0 +1,23 @@
+package sub
+
+import (
+	"live/src/utils"
+	"strings"
+)
+
+func GetSub(topic string) ([]*Node, []string) {
+	topicSlice := strings.Split(topic, "/")
+	nodes := GetWildCards(topicSlice)
+	clients := GetHashSub(topic)
+	return nodes, clients
+}
+
+func Sub(topic, clientIdentifier string, topicSlice []string) {
+
+	if utils.HasWildcard(topicSlice) {
+		AddTreeSub(topicSlice, clientIdentifier)
+	} else {
+		AddHashSub(topic, clientIdentifier)
+	}
+
+}
